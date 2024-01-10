fb0629378060fdef3e95a012bd607a312f47bfe4
Jeffrey <matheusunitt@gmail.com>
2024-01-10 09:59:58 -0300

    Adiciona esqueleto básico da página para criar artigos; Adiciona esqueleto básico da página index

---
+++ src/app/criar/page.tsx
@@ -0,0 +1,9 @@
+const Criar = () => {
+  return (
+    <main className="rounded-lg border-2 border-slate-200 p-8">
+      <h1 className="text-3xl text-sky-700 font-bold">Criar novo artigo</h1>
+    </main>
+  );
+};
+
+export default Criar;
