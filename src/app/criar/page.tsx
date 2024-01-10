5687a70de7b4a77442abf252b53adfdc480a549b
Jeffrey <matheusunitt@gmail.com>
2024-01-10 11:06:14 -0300

    Adiciona botões na página de Criar artigo

--- src/app/criar/page.tsx
+++ src/app/criar/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 import Box from "../../components/Box";
 
 const Criar = () => {
-  return <Box title="Criar novo artigo" />;
+  return (
+    <Box title="Criar novo artigo">
+      <div className="flex flex-col gap-1 w-full sm:flex-row">
+        <Link
+          href="/"
+          className="px-4 py-2 rounded-lg text-center bg-white border border-slate-300 text-black hover:bg-gray-100"
+        >
+          Cancelar
+        </Link>
+        <button className="px-4 py-2 rounded-lg text-center bg-black text-white hover:opacity-85">
+          Criar
+        </button>
+      </div>
+    </Box>
+  );
 };
 
 export default Criar;
