2a6f5ff88ad86902833935c9e10c777bc62c7919
Jeffrey <matheusunitt@gmail.com>
2024-01-11 10:55:07 -0300

    Reorganiza o componente Box

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,16 +1,23 @@
 const Box = ({
   title,
+  subtitle,
   children,
 }: {
-  title: string;
+  title: string | undefined;
+  subtitle: string | undefined;
   children: React.ReactNode;
 }) => {
   return (
     <div className="rounded-lg border-2 border-slate-200 p-8">
-      <h1 className="text-3xl text-black font-bold border-b-2 border-slate-100 py-4 mb-4">
-        {title}
-      </h1>
-      <div className="flex flex-col gap-10 w-full">
+      <header className="border-b pb-6 mb-6">
+        <h1 className="text-3xl text-black font-bold">{title}</h1>
+        {subtitle ? (
+          <span className="inline-block mt-4 text-gray-500 text-sm italic">
+            {subtitle}
+          </span>
+        ) : null}
+      </header>
+      <div className="flex flex-col gap-6 w-full">
         {children ? children : null}
       </div>
     </div>
