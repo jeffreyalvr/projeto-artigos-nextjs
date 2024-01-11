0253ab0eee5aad42f24b08a162547d20ab317e86
Jeffrey <matheusunitt@gmail.com>
2024-01-11 09:37:18 -0300

    Adiciona migration inicial

---
+++ prisma/migrations/20240111123616_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Artigo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Artigo_pkey" PRIMARY KEY ("id")
+);
