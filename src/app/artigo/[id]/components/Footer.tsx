0e2b81c1a8829eb304a4ea4c8a0c8c9ea197d6ff
Jeffrey <matheusunitt@gmail.com>
2024-01-13 09:57:33 -0300

    Adiciona ícones nos links

--- src/app/artigo/[id]/components/Footer.tsx
+++ src/app/artigo/[id]/components/Footer.tsx
@@ -1,4 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import facebook_img from "/public/icons/facebook.png";
+import whatsapp_img from "/public/icons/whatsapp.png";
+import telegram_img from "/public/icons/telegram.png";
+import x_img from "/public/icons/x.png";
 
 const Footer = ({
   descricaoParaCompartilhamento,
@@ -8,31 +14,49 @@ const Footer = ({
   url: string;
 }) => {
   return (
-    <footer className="flex flex-row flex-wrap items-center gap-2 border-t pt-6">
+    <footer className="flex flex-row flex-wrap items-center gap-3 border-t pt-6">
       <span className="text-gray-500">Compartilhar:</span>
       <Link
         href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
-        className="inline-flex px-4 py-1 rounded-lg bg-zinc-200 hover:opacity-75"
+        className="inline-flex"
       >
-        Facebook
+        <Image
+          src={facebook_img}
+          width="24"
+          height="24"
+          alt="Facebook"
+          title="Facebook"
+        />
       </Link>
       <Link
         href={`https://api.whatsapp.com/send?text=${descricaoParaCompartilhamento}`}
-        className="inline-flex px-4 py-1 rounded-lg bg-zinc-200 hover:opacity-75"
+        className="inline-flex"
       >
-        WhatsApp
+        <Image
+          src={whatsapp_img}
+          width="24"
+          height="24"
+          alt="WhatsApp"
+          title="WhatsApp"
+        />
       </Link>
       <Link
         href={`https://t.me/share/url?url=${url}&text=${descricaoParaCompartilhamento}`}
-        className="inline-flex px-4 py-1 rounded-lg bg-zinc-200 hover:opacity-75"
+        className="inline-flex"
       >
-        Telegram
+        <Image
+          src={telegram_img}
+          width="24"
+          height="24"
+          alt="Telegram"
+          title="Telegram"
+        />
       </Link>
       <Link
         href={`https://twitter.com/intent/tweet?text=${descricaoParaCompartilhamento}`}
-        className="inline-flex px-4 py-1 rounded-lg bg-zinc-200 hover:opacity-75"
+        className="inline-flex"
       >
-        X
+        <Image src={x_img} width="24" height="24" alt="X" title="X" />
       </Link>
     </footer>
   );
